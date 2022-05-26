eb52412fd194ffae741d0754a2fb6d2b88bf170e
Cengiz Demir <kontakt.cengiz.demir@gmail.com>
2022-05-26 23:30:46 +0200

    26.05.2022

--- src/test/java/testMitThemen/LoginUserTest.java
+++ src/test/java/testMitThemen/LoginUserTest.java
@@ -2,6 +2,8 @@ package testMitThemen;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import utilities.TestBase;
 
@@ -12,18 +14,27 @@ public class LoginUserTest extends TestBase {
         //2. Navigate to url 'http://automationexercise.com'
         driver.navigate().to("http://automationexercise.com");
         //3. Verify that home page is visible successfully
-        String urlExpected="http://automationexercise.com";
-       // String actualUrl =driver.getCurrentUrl();
+        WebElement home = driver.findElement(By.xpath("(//a[@href='/'])[2]"));
+        Assert.assertTrue(home.isDisplayed());
+        // String actualUrl =driver.getCurrentUrl();
         //Assert.assertEquals(urlExpected,actualUrl);
 
 
-
         //4. Click on 'Signup / Login' button
+        WebElement login = driver.findElement(By.xpath("//a[@href='/login']"));
+        login.click();
+
         //5. Verify 'Login to your account' is visible
+        WebElement loginWriter = driver.findElement(By.xpath("//*[text()='Login to your account']"));
+        Assert.assertTrue(loginWriter.isDisplayed());
         //6. Enter correct email address and password
+        WebElement email = driver.findElement(By.xpath("//*[@*='email']"));
+        email.sendKeys("@mail.com");
+        WebElement password = driver.findElement(By.xpath("//*[@*='password']"));
+        password.sendKeys("124");
         //7. Click 'login' button
-        //8. Verify that 'Logged in as username' is visible
-        //9. Click 'Delete Account' button
-        //10. Verify that 'ACCOUNT DELETED!' is visible
+        WebElement login2 = driver.findElement(By.xpath("//*[text()='Login']"));
+        login2.click();
+
     }
 }
