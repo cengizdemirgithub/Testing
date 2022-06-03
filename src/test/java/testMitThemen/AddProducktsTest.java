2939f1d8a0a7379ba199e6ec830a454f485305f2
Cengiz Demir <kontakt.cengiz.demir@gmail.com>
2022-06-03 23:47:15 +0200

    03.06.2022

--- src/test/java/testMitThemen/AddProducktsTest.java
+++ src/test/java/testMitThemen/AddProducktsTest.java
@@ -1,20 +1,34 @@
 package testMitThemen;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import utilities.TestBase;
 
 public class AddProducktsTest extends TestBase {
     @Test
-    public void AddProducktsTest() {
+    public void AddProducktsTest() throws InterruptedException {
         //1. Launch browser
         //2. Navigate to url 'http://automationexercise.com'
-        //3. Verify that home page is visible successfully
-        //4. Click 'Products' button
-        //5. Hover over first product and click 'Add to cart'
-        //6. Click 'Continue Shopping' button
-        //7. Hover over second product and click 'Add to cart'
-        //8. Click 'View Cart' button
-        //9. Verify both products are added to Cart
-        //10. Verify their prices, quantity and total price
+        driver.navigate().to("http://automationexercise.com");
+        //Click 'Products' button
+        driver.findElement(By.xpath("//a[@*='/products']")).click();
+        //Hover over first product and click 'Add to cart'
+        driver.findElement(By.xpath("(//*[@*='btn btn-default add-to-cart'])[1]")).click();
+        //Click 'Continue Shopping' button
+        driver.findElement(By.xpath("//*[text()='Continue Shopping']")).click();
+        //Hover over second product and click 'Add to cart'
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("(//*[@*='btn btn-default add-to-cart'])[2]")).click();
+        //Click 'View Cart' button
+        driver.findElement(By.xpath("//*[text()='View Cart']")).click();
+        //Verify both products are added to Cart
+        String resource = driver.getPageSource();
+        Thread.sleep(3000);
+        WebElement product1=driver.findElement(By.xpath("(//a[@class='btn btn-default add-to-cart'])[1]"));
+        WebElement product2=driver.findElement(By.xpath("(//*[@*='btn btn-default add-to-cart'])[2]"));
+        Assert.assertTrue(resource.contains(product1.getText()));
+        //Verify their prices, quantity and total price
     }
 }
